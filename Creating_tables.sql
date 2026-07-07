700f19fa8f6df13a64d528d1fab9355ff56ecb5f
Lance Pery <lpery@academic.rrc.ca>
2026-07-06 21:03:34 -0500

    added the show table.

--- Creating_tables.sql
+++ Creating_tables.sql
@@ -10,12 +10,29 @@ CREATE TABLE Movie
 	movie_name VARCHAR(25) NOT NULL,
 	movie_rating NUMERIC,
 	release_date DATE,
-	director VARCHAR(25),
+	overview VARCHAR(MAX),
+	creator VARCHAR(25),
 	time_minutes NUMERIC,
 	budget NUMERIC,
 	
 	CONSTRAINT movie_id_unique
 		UNIQUE (movie_id),
-	CONSTRAINT over_0_rating
+	CONSTRAINT over_0_movie_rating
 		CHECK (movie_rating > 0)
+);
+
+CREATE TABLE Show
+(
+	show_id NUMERIC UNIQUE NOT NULL,
+	show_name VARCHAR(50) NOT NULL,
+	show_rating NUMERIC,
+	release_date DATE,
+	overview VARCHAR(MAX),
+	seasons NUMERIC,
+	budget NUMERIC,
+
+	CONSTRAINT show_id_unique
+		UNIQUE (show_id),
+	CONSTRAINT over_0_show_rating
+		CHECK (show_rating > 0)
 );
