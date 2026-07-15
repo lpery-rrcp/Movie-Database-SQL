b510bcd4fbba757195a6bbb3bc793da25868279a
Lance Pery <lpery@academic.rrc.ca>
2026-07-15 16:32:45 -0500

    Access the popular movies confirm.

--- movie_insert.py
+++ movie_insert.py
@@ -6,4 +6,12 @@ from dotenv import load_dotenv
 import requests
 
 load_dotenv()
-print(f"TMDB_API_KEY: {os.getenv('TMDB_API_KEY')}")
+API_KEY = os.getenv("TMDB_API_KEY")
+
+URL = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}"
+
+response = requests.get(URL)
+movies = response.json()["results"]
+
+for movie in movies:
+    print(f"{movie['id']}, {movie['title']}")
