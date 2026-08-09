45309fcee9df6092435225066bff7be6399ffc49
Lance Pery <lpery@academic.rrc.ca>
2026-08-08 19:52:56 -0500

    Have access to the details of the shows.

--- Data/show_insert.py
+++ Data/show_insert.py
@@ -29,9 +29,48 @@ def show_shows():
         print(f"- {show['name']} (ID: {show['id']})")
 
 
-# Fucntion calls
-show_shows()
+def insert_shows_table():
+    URL = f"{BASE_URL}/tv/popular?api_key={API_KEY}"
+    response = requests.get(URL)
+    shows = response.json()["results"]
 
+    for show in shows:
+        show_id = show["id"]
+        show_name = show["name"]
+
+        # Fetch additional details for each show
+        details_url = f"{BASE_URL}/tv/{show_id}?api_key={API_KEY}"
+        details_response = requests.get(details_url)
+        details = details_response.json()
+
+        # Extract additional details
+        show_rating = details.get("vote_average")
+        release_date = details.get("first_air_date")
+        overview = details.get("overview")
+        seasons = details.get("number_of_seasons")
+        budget = details.get("budget")
+        episodes = details.get("number_of_episodes")
+        print(f"Show ID: {show_id}, Name: {show_name}, Rating: {show_rating}, Release Date: {release_date}, Overview: {overview}, Seasons: {seasons}, Budget: {budget}, Episodes: {episodes}")
+
+        # # Check if the show already exists in the database
+        # cursor.execute(
+        #     "SELECT COUNT(*) FROM Show WHERE id = ?;", (show_id,)
+        # )
+        # count = cursor.fetchone()[0]
+        # if count == 0:
+        #     cursor.execute(
+        #         "INSERT INTO Show (id, show_name) VALUES (?, ?, ?, ?, ?);",
+        #         (show_id, show_name, show_rating, release_date, overview, seasons, budget)
+        #     )
+        #     print(f"Inserting: {show_name} (ID: {show_id})")
+        # else:
+        #     print(f"Skipped (already exists): {show_name} (ID: {show_id})")
+        #     continue
+
+
+# Function calls
+# show_shows()
+insert_shows_table()
 # Close the cursor and connection
 conn.commit()
 
